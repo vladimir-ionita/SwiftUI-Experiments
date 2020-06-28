c9b9f2728db3f718d4c25bdee4fd3884da565d7a
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-28 15:30:10 -0500

    Add `MapView` and `CircleImage` to `ContentView`

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,16 +10,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-            }
-        }.padding()
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
+            }.padding()
+            
+            Spacer()
+        }
     }
 }
 
