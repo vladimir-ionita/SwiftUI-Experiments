e5751dad20d39757dd72d2f3cb9e50fef603ccf0
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-28 15:06:55 -0500

    Create `CircleImage`

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Marina Ionita on 28/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
