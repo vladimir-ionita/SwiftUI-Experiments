e04185fb385ac076dc1e808690e92e4eac8d734d
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 16:11:58 -0500

    Draw a hexagon in the `Badge`

--- Landmarks/Landmarks/Views/Badge.swift
+++ Landmarks/Landmarks/Views/Badge.swift
@@ -10,7 +10,37 @@ import SwiftUI
 
 struct Badge: View {
     var body: some View {
-        Text("Badge")
+        Path { path in
+            let width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * (0.20 + HexagonParameters.adjustment)
+                )
+            )
+            
+            HexagonParameters.points.forEach {
+                path.addLine(
+                    to: .init(
+                        x: width * $0.useWidth.0 * $0.xFactors.0,
+                        y: height * $0.useHeight.0 * $0.yFactors.0
+                    )
+                )
+                
+                path.addQuadCurve(
+                    to: .init(
+                        x: width * $0.useWidth.1 * $0.xFactors.1,
+                        y: height * $0.useHeight.1 * $0.yFactors.1
+                    ),
+                    control: .init(
+                        x: width * $0.useWidth.2 * $0.xFactors.2,
+                        y: height * $0.useHeight.2 * $0.yFactors.2
+                    )
+                )
+            }
+        }
+        .fill(Color.black)
     }
 }
 
