803e13ebc22f9bad9830ef8d95c83f1a39fe3e08
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 15:31:20 -0500

    Use the `UserData` environment object in `LandmarkDetail`

--- Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -9,7 +9,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var userData: UserData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         VStack {
