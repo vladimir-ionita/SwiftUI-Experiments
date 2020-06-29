d5cab94981c8ccc0d392e3b1ad23753187cf0a16
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-28 19:17:54 -0500

    Boyscout

--- Landmarks/Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -19,10 +19,10 @@ struct LandmarkDetail: View {
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
-            
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+                
                 HStack {
                     Text("Joshua Tree National Park")
                         .font(.subheadline)
