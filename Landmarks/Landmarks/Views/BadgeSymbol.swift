b320d86b47fb8aa44f0ed256897ee1b91f14ba7d
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 16:26:49 -0500

    Create `BadgeSymbol`

---
+++ Landmarks/Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Marina Ionita on 29/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text("Badge Symbol")
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
