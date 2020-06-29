4b684711abcc5b6b34408c2618fa9914ab049e55
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 15:18:12 -0500

    Create `UserData`

---
+++ Landmarks/Landmarks/Models/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Marina Ionita on 29/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class UserData: ObservableObject {
+    @Published var showFavoritesOnly = false
+    @Published var landmarks = landmarkData
+}
