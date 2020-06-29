a93966532333ce739b214119d799f792290fb1a4
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 16:11:18 -0500

    Create `Badge` view

---
+++ Landmarks/Landmarks/Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Marina Ionita on 29/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
