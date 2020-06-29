ea3b0ab54a9b3170e9012f647ce3ef917f77ef38
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 16:51:17 -0500

    Create `RotatedBadgeSymbol`

---
+++ Landmarks/Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Marina Ionita on 29/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol()
+    }
+}
