69e5fcc22ad0d9d17622106597c2c169f3ecbaf3
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 20:32:10 -0500

    Create `ProfileSummary`

---
+++ Landmarks/Landmarks/Views/Profile/ProfileSummary.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by Vladimir Ionita on 29/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    var profile: Profile
+    
+    static let goalDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    var body: some View {
+        List {
+            Text(profile.username)
+                .bold()
+                .font(.title)
+            
+            Text("Notifications: \(self.profile.prefersNotifications ? "On": "Off" )")
+            
+            Text("Seasonal Photos: \(self.profile.seasonalPhoto.rawValue)")
+            
+            Text("Goal Date: \(self.profile.goalDate, formatter: Self.goalDateFormatter)")
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+    }
+}
