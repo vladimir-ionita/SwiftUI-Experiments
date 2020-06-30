08d0559cced00af9676cc20c3b44f0313d143988
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 21:18:36 -0500

    Create `ProfileEditor`

---
+++ Landmarks/Landmarks/Views/Profile/ProfileEditor.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Vladimir Ionita on 29/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
