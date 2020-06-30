df87c0245b83d4dcd1be0e4d2f96c83c7cd53552
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-06-29 20:43:34 -0500

    Add `Hike`

--- Landmarks/Landmarks/Models/Data.swift
+++ Landmarks/Landmarks/Models/Data.swift
@@ -6,18 +6,20 @@
 //  Copyright © 2020 Vladimir Ionita. All rights reserved.
 //
 
+import Foundation
+import CoreLocation
 import UIKit
 import SwiftUI
-import CoreLocation
 
 let landmarkData: [Landmark] = load("landmarkData.json")
+let hikeData: [Hike] = load("hikeData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
     
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-        else {
-            fatalError("Couldn't find \(filename) in main bundle.")
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
     }
     
     do {
---
+++ Landmarks/Landmarks/Models/Hike.swift
@@ -0,0 +1,33 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Vladimir Ionita on 29/06/2020.
+//  Copyright © 2020 Vladimir Ionita. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct Hike: Codable, Hashable, Identifiable {
+    var name: String
+    var id: Int
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        return Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
